13755c1fff1e510eef55a2e8eec6df5202a2c51f
Joshusan <leitzminolta+mstdn@gmail.com>
2017-07-04 22:37:15 +0900

    .gitignore のミスを直した

--- plugins/mode.py
+++ plugins/mode.py
@@ -2,8 +2,8 @@ import os.path
 
 
 dev    = os.path.exists(".dev")
-mode   = os.path.exists(".card")
+card   = os.path.exists(".card")
 sushi  = os.path.exists(".sushi")
-uranai = not any([mode, sushi])
+uranai = not any([card, sushi])
 
 test_prefix = "^test " if dev else "^(?!test ).*"
