ca05a74833aa28fe7ac08a62820d9d6ae2f2eefe
Joshusan <leitzminolta+mstdn@gmail.com>
2017-07-22 15:36:27 +0900

    Web API を直接叩かずに slacker を呼び出す

--- plugins/api.py
+++ plugins/api.py
@@ -3,38 +3,43 @@ import os.path
 import re
 import requests
 import slackbot_settings
+from slacker import Slacker
 
+slackapi = Slacker(slackbot_settings.API_TOKEN)
 
-def get_username(user_id):
+def get_user(user_id):
+    response = slackapi.users.info(user_id)
+    return response.body["user"]
+
+def get_channel(channel_id):
+    response = slackapi.channels.info(channel_id)
+    return response.body["channel"]
+
+def get_user_mame(user_id):
     emoji = re.compile(r":.+?:")
-    params = {
-        'token' : slackbot_settings.API_TOKEN,
-        'user'  : user_id
-    }
-    res  = requests.post('https://slack.com/api/users.profile.get', data=params)
-    user = res.json()["profile"]
-    first, last = user["first_name"], user["last_name"]
-    last = emoji.sub('', last)
-    return " ".join([first, last]) if last else first
+    user = get_user(user_id)
+    return emoji.sub('', user["profile"]["real_name"])
 
+def get_channel_name(channel_id):
+    channel = get_channel(channel_id)
+    return channel["name"]
 
 def post_image(message, pillow_image, title=None, comment=None, file_name=None):
-    output = io.BytesIO()
+    def filename_to_filetype(file_name):
+        root, ext = os.path.splitext(file_name or 'sample.png')
+        file_type = ext[1:] if ext else 'png'
+        return file_type if file_type != 'jpg' else 'jpeg'
+
+    output  = io.BytesIO()
     pillow_image.save(output, filename_to_filetype(file_name), quality=100)
-    params = {
-        'token'           : slackbot_settings.API_TOKEN,
-        'channels'        : message.channel._body['id'],
-        'title'           : title,
-        'initial_comment' : comment
+    data = {
+        'filename': file_name,
+        'title': title,
+        'initial_comment': comment,
+        'channels': message.body['channel']
     }
-    file_object = {
-        'file' : (file_name, output.getvalue())
+    files = {
+        "file": output.getvalue()
     }
-    requests.post('https://slack.com/api/files.upload', data=params, files=file_object)
+    slackapi.files.post('files.upload', data=data, files=files)
 
-
-def filename_to_filetype(file_name):
-    file_name = file_name or 'sample.png'
-    root, ext = os.path.splitext(file_name)
-    file_type = ext[1:] if ext else 'png'
-    return file_type if file_type != 'jpg' else 'jpeg'
